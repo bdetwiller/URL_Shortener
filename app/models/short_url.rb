d3774aa49baf494fc66fe1996c5684e01446b54d
David+Luke <david+luke@appacademy.io>
2013-04-01 18:46:07 -0700

    debugging

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -5,52 +5,23 @@ class ShortUrl < ActiveRecord::Base
   has_many :tags, :through => :taggings
   attr_accessible :url, :user_id, :long_id
 
-  def self.add(long_url, screen_name)
-    user_id = ShortUrl.name_to_id(screen_name)
+  def self.add(long_url, user)
     link = LongUrl.where(:url => long_url)
     if link == []
       long_url_id = LongUrl.create(:url => long_url).id
     else
       long_url_id = link.first.id
-      if ShortUrl.where(:long_id => long_url_id, :user_id => user_id)
+      if ShortUrl.where(:long_id => long_url_id, :user_id => user.id)
         raise 'You have already created a link for this URL.'
       end
     end
     short_url = "http://lande.com/#{SecureRandom.urlsafe_base64}"
-    ShortUrl.create(:url => short_url, :long_id => long_url_id, :user_id => user_id)
+    ShortUrl.create(:url => short_url, :long_id => long_url_id, :user_id => user.id)
   end
 
-  def self.launch(short_url, screen_name)
-    sql_query = ShortUrl.where(:url => short_url).first
-    user_id = name_to_id(screen_name)
-    if sql_query
-      long = LongUrl.where(:id => sql_query.long_id).first
-      Launchy.open(long.url)
-      Visit.create(:user_id => user_id, :short_url_id => sql_query.id)
-    else
-      raise 'That short url is not in the database.'
-    end
-  end
-
-  def print_stats
-    visitors = Visit.where(:short_url_id => self.id)
-    current_time = Time.now - 600
-    past_ten_min = Visit.where("created_at >= ?", current_time)
-
-    puts "Number of Visitors : #{visitors.count}"
-    puts "Number of Unique Visitors : #{visitors.uniq.count}"
-    puts "Visitors in past ten minutes"
-    puts past_ten_min
-  end
-
-  def self.name_to_id(screen_name)
-    User.where(:screen_name => screen_name).first.id
-  end
-
-  def self.find(long_url, screen_name)
-    user_id = ShortUrl.name_to_id(screen_name)
+  def self.find(long_url, user)
     long_id = LongUrl.where(:url => long_url).first.id
-    ShortUrl.where(:long_id => long_id, :user_id => user_id)
+    ShortUrl.where(:long_id => long_id, :user_id => user.id)
   end
 
 end
--- script/url_shortener.rb
+++ script/url_shortener.rb
@@ -1,15 +1,21 @@
 def run
   user = get_user
-  input = get_input
-  case input
-  when 1
-    add_url(user)
-  when 2
-    launch(user)
-  when 3
-    add_comment(user)
-  when 4
-  when 5
+  loop do
+    input = get_input
+    case input
+    when 1
+      add_url(user)
+    when 2
+      launch(user)
+    when 3
+      add_comment(user)
+    when 4
+      print_stats
+    when 5
+      print_user_links
+    when 6
+      break
+    end
   end
 end
 
@@ -33,18 +39,25 @@ def get_input
   puts "3. Add comment"
   puts "4. Print URL stats"
   puts "5. Print user's links"
+  puts "6. Quit"
   valid_input = false
   until valid_input
     input = gets.chomp.scan(/\d/)
     valid_input = true if input.length == 1
   end
-  input.to_i
+  input.first.to_i
 end
 
 def add_url(user)
   puts "Enter the URL you would like shortened:"
   long_url = gets.chomp
-  ShortUrl.add(long_url, user)
+  puts "Allowed tags:"
+  puts ["Dogs", "Cats", "Puppies", "Kittens"]
+  puts "Enter tag for your link:"
+  input_tag = gets.chomp
+  short_url = ShortUrl.add(long_url, user)
+  puts short_url.url
+  #Tag.create()
 end
 
 def launch(user)
@@ -52,6 +65,7 @@ def launch(user)
   input_url = gets.chomp
   short_url = ShortUrl.find_by_url(input_url)
   if short_url
+    print_comments(short_url)
     long_url = LongUrl.find_by_id(short_url.long_id)
     Launchy.open(long_url.url)
     Visit.create(:user_id => user.id, :short_url_id => short_url.id)
@@ -60,7 +74,53 @@ def launch(user)
   end
 end
 
+def print_comments(short_url)
+  Comment.where(:short_url_id => short_url.id).each do |comment|
+    puts comment.body
+  end
+end
+
 def add_comment(user)
   puts "Which link would you like to comment on?"
-  short_url = gets.chomp
+  input_url = gets.chomp
+  short_url = ShortUrl.find_by_url(input_url)
+  if short_url == nil
+    raise "No such link!"
+  else
+    puts "Enter comment:"
+    body = gets.chomp
+    Comment.create(:user_id => user.id, :short_url_id => short_url.id,
+                   :body => body)
+    puts "Comment recorded."
+  end
 end
+
+def print_stats
+  puts "Enter URL to print stats for:"
+  input_url = gets.chomp
+  short_url = ShortUrl.find_by_url(input_url)
+  visitors = Visit.where(:short_url_id => short_url.id)
+  current_time = Time.now - 600
+  past_ten_min = Visit.where("created_at >= ?", current_time)
+
+  puts "Number of Visitors : #{visitors.count}"
+  puts "Number of Unique Visitors : #{visitors.uniq.count}"
+  puts "Visitors in past ten minutes:"
+  puts past_ten_min
+end
+
+def print_user_links
+  puts "Enter user to print links for:"
+  input_user = gets.chomp
+  user = User.find_by_screen_name(input_user)
+  if user.nil?
+    raise "No such user!"
+  else
+    ShortUrl.where(:user_id => user.id).each do |short_url|
+      puts "Short form: #{short_url.url}"
+      puts "Long form: #{LongUrl.find_by_id(short_url.long_id).url}"
+    end
+  end
+end
+
+run
